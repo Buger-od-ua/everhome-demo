7728399d5c105a729faf3a4c0fc2df688bbf23cf
buger <buheriaoleksii@gmail.com>
2019-12-11 19:46:41 +0200

    Initial commit

---
+++ src/main/java/com/budova/everhome/EverhomeApplication.java
@@ -0,0 +1,13 @@
+package com.budova.everhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budova/everhome/EverhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budova.everhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
